85668fb61e9a6a7fb29ee45a25a7a7fcb751d734
Trishka <snowflak@yandex.ru>
2020-08-14 11:38:39 +0300

    create reducer with INC, DEC, RESET

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// редюсер
+const reducer = (state=0, action) => {
+  switch(action.type) {
+    case 'INC':
+      return state + 1;
+    case 'DEC':
+      return state - 1;
+    case 'RESET':
+      return 0;
+    default:
+      return state;
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//  создаем хранилище
+const store = createStore(reducer);
+
+//  функции action-creator
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const reset = (value) => ({type: 'RESET', value});
+
+
+//  Проверяем нажатия кнопок
+document.getElementById('ink').addEventListener('click', () => {
+  store.dispatch(inc());
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec());
+});
+
+document.getElementById('reset').addEventListener('click', () => {
+  store.dispatch(reset());
+});
+
+//  функция обновляет counter
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
+
+// при каждом изменении
+store.subscribe(update);
+
